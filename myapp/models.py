8042c2ce71e2d9630e4121252d35bec0b5eb4863
marketitos <canteromarcoset36@gmail.com>
2023-04-20 09:49:53 -0300

    Actualizasao

--- myapp/models.py
+++ myapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Cards(models.Model):
-    image = models.ImageField(blank=True, upload_to='images')
+    image = models.ImageField(null=True, blank=True, upload_to='images')
     titulo = models.CharField(max_length=200)
     price = models.IntegerField()
     def _str_(self):
