4c6596174c268954443b388bd4155963177a83b1
marketitos <canteromarcoset36@gmail.com>
2023-04-27 11:05:22 -0300

    Sidebar

--- myapp/models.py
+++ myapp/models.py
@@ -5,6 +5,9 @@ from django.db import models
 
 ## Crear un models que se llame producto con una foreign key de titulo y precio para poder hacer el carrito
 
+#Hacer qeu las cartas no se acoplen y se pongan abajo 
+#arreglar la sidebar
+
 class Cards(models.Model):
     image = models.ImageField(null=True, blank=True, upload_to='images')
     titulo = models.CharField(max_length=200)
