8e166a406f81f9f9d41991fcf230f089c348ca86
marketitos <canteromarcoset36@gmail.com>
2023-05-01 22:26:44 -0300

    Presentacion parcial

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,13 +115,16 @@ USE_TZ = True
 
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+
 
 LOGIN_REDIRECT_URL = 'kiosco'
 LOGOUT_REDIRECT_URL = 'signin'
+STATIC_URL = "/static/"
 
 MEDIA_URL='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
