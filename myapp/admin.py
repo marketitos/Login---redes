50e2b3282c23fc2f0164ce9df7208a6453b2cca6
marketitos <canteromarcoset36@gmail.com>
2023-05-29 10:33:16 -0300

    asd

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Cards
+from .models import Categoria
 # Register your models here.
 admin.site.register(Cards)
+admin.site.register(Categoria)
--- myapp/models.py
+++ myapp/models.py
@@ -12,8 +12,12 @@ class Cards(models.Model):
     image = models.ImageField(null=True, blank=True, upload_to='images')
     titulo = models.CharField(max_length=200)
     price = models.IntegerField()
+
     def _str_(self):
         return self.titulo
     
     def __str__(self):
         return f'{self.nombre} -> {self.precio}'
+
+class Categoria(models.Model):
+    categoria = models.CharField(max_length=200)        
